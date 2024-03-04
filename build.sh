b33cf1a495534dfd688ea1e5641eebbb8658bb1c
indreshrsingh <105886475+indreshrsingh@users.noreply.github.com>
2024-03-05 04:08:52 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
  echo "BUILD START"
  python3.9 -m pip install -r requirements.txt
- mkdir -p /app/static
+ mkdir -p /training_website/static
  python manage.py collectstatic && gunicorn --workers 2 myproject.wsgi
  python manage.py makemigrations && migrate
  echo "BUILD END"

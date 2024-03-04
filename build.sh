fc8cf95a9692c6b8c7f3ea278a4f7e64df11fe74
indreshrsingh <105886475+indreshrsingh@users.noreply.github.com>
2024-03-05 01:25:55 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
  echo "BUILD START"
  python3.9 -m pip install -r requirements.txt
+ sudo apt install python3.9-dev
  python manage.py collectstatic && gunicorn --workers 2 myproject.wsgi
  python3.9 -m pip install psycopg2
  python manage.py makemigrations && migrate

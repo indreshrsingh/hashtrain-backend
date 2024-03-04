b9c3bd5fc8f57cfbb676b288ee39b17964931720
indreshrsingh <105886475+indreshrsingh@users.noreply.github.com>
2024-03-05 05:25:23 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
  mkdir -p /training_website/static
  python3.9 manage.py collectstatic 
  python manage.py gunicorn --workers 2 myproject.wsgi
+ python myapp/manage.py collectstatic --noinput; bin/gunicorn_django --workers=4 --bind=0.0.0.0:$PORT myapp/settings.py
  python manage.py makemigrations && migrate
  echo "BUILD END"

f6ae4ff177914cf3b7c832d193053760a6c520e9
indreshrsingh <105886475+indreshrsingh@users.noreply.github.com>
2024-03-05 03:51:16 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
  echo "BUILD START"
  python3.9 -m pip install -r requirements.txt
- sudo apt install python3.9-dev
- $ sudo apt-get install libpq-dev
  python manage.py collectstatic && gunicorn --workers 2 myproject.wsgi
- python3.9 -m pip install psycopg2-binary
  python manage.py makemigrations && migrate
  echo "BUILD END"

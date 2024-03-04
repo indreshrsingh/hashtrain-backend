b69bfa4cbfacce2c3d39c4163c6a381f33431241
indreshrsingh <105886475+indreshrsingh@users.noreply.github.com>
2024-03-05 05:10:58 +0530

    Update settings.py

--- training_website/settings.py
+++ training_website/settings.py
@@ -169,7 +169,6 @@ STORAGES = {
 
 
 }
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

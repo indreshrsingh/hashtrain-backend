e5ff769c707bae968627a2b536776ac41b8dc000
indreshrsingh <105886475+indreshrsingh@users.noreply.github.com>
2024-03-05 01:10:29 +0530

    Update urls.py

--- training_website/urls.py
+++ training_website/urls.py
@@ -26,5 +26,4 @@ urlpatterns = [
     path('', include('faq.urls')),
     path('', include('invoice.urls')),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+

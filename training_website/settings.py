cb2f042a8a51deadf57f683a9426162f58ce7f9c
indreshrsingh <105886475+indreshrsingh@users.noreply.github.com>
2024-03-06 01:19:58 +0530

    Update settings.py

--- training_website/settings.py
+++ training_website/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 

ddd5f59df63d1cb13c9107ba649db62014469a69
indreshrsingh <105886475+indreshrsingh@users.noreply.github.com>
2024-03-06 02:00:37 +0530

    Update settings.py

--- training_website/settings.py
+++ training_website/settings.py
@@ -154,7 +154,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-STATICFILES_STORAGE = 'my_project.storage.WhiteNoiseStaticFilesStorage'
+STATICFILES_STORAGE = 'training_website.storage.WhiteNoiseStaticFilesStorage'
 
 
 # Default primary key field type

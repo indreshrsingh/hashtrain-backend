c7c5041e6aed886818deb18aaf30959edf070acf
indreshrsingh <105886475+indreshrsingh@users.noreply.github.com>
2024-03-05 05:59:08 +0530

    Update settings.py

--- training_website/settings.py
+++ training_website/settings.py
@@ -160,7 +160,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-SSTATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STORAGES = {
     # ...
     "staticfiles": {

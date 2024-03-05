6d08adc773f1a576264ad4ab4629a9bc4e8ab7d5
indreshrsingh <105886475+indreshrsingh@users.noreply.github.com>
2024-03-06 01:37:38 +0530

    Update settings.py

--- training_website/settings.py
+++ training_website/settings.py
@@ -154,7 +154,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 
 # Default primary key field type

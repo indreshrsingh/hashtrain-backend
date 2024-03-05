fe53f5021e6df742a4d56e82a8ca95d34a1dabdc
indreshrsingh <105886475+indreshrsingh@users.noreply.github.com>
2024-03-05 19:23:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
  echo "BUILD START"
 pip install -r requirements.txt
-mkdir -p /training_website/static
 python manage.py collectstatic
-python manage.py runserver --workers 2
 python manage.py migrate
  echo "BUILD END"

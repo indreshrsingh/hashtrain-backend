8cc1ca8a74c8c603bb0e288e77e39329509b575d
indreshrsingh <105886475+indreshrsingh@users.noreply.github.com>
2024-03-07 15:44:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
  echo "BUILD START"
 pip install -r requirements.txt
-python3.11 manage.py migrate
-python3.11 manage.py collectstatic --noinput
+python3.9 manage.py migrate
+python3.9 manage.py collectstatic --noinput
  echo "BUILD END"

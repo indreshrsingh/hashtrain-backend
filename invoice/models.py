f6d0f0550aea5af821a3e0b6be08dedadc7f2722
indreshrsingh <105886475+indreshrsingh@users.noreply.github.com>
2024-03-09 10:20:13 +0530

    Update models.py

--- invoice/models.py
+++ invoice/models.py
@@ -1,13 +1,13 @@
+# models.py
 from django.db import models
 
 class Invoice(models.Model):
     customer_name = models.CharField(max_length=100)
     training_id = models.IntegerField(default=0)
+    company_name = models.CharField(max_length=100)
     training_name = models.CharField(max_length=100)
     training_description = models.TextField()
-    company_name = models.CharField(max_length=100)
+    pdf_link = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.training_name} - {self.customer_name}"
-    
-    
